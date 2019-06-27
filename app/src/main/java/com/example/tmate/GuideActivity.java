b18256ae3c2fdcec52a3e8d4eb95ca7275461ed2
sangmin95 <sangmin95@gmail.com>
2019-06-28 05:31:15 +0900

    Modify UI

--- app/src/main/java/com/example/tmate/GuideActivity.java
+++ app/src/main/java/com/example/tmate/GuideActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -13,6 +14,9 @@ public class GuideActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //상단바 제거
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_guide);
 
         Button skipButton = findViewById(R.id.guide_btn_skip);
