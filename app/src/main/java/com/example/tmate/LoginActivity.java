544fe681bc15331d4c6b4eaaea6a2324ff0e37b2
Gwangjo Gong <dwc05101@kaist.ac.kr>
2019-06-27 14:33:44 +0900

    API

--- app/src/main/java/com/example/tmate/LoginActivity.java
+++ app/src/main/java/com/example/tmate/LoginActivity.java
@@ -3,14 +3,33 @@ package com.example.tmate;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.kakao.auth.ISessionCallback;
+import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.LoginButton;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
+import com.kakao.util.exception.KakaoException;
+
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = LoginActivity.class.getSimpleName();
+
+    private LoginButton btn_kakao_login;
+    private com.facebook.login.widget.LoginButton btn_fb_login;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,28 +37,58 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         LoginButtonClickListener loginButtonClickListener = new LoginButtonClickListener();
-        Button btn_fb = findViewById(R.id.login_btn_fb);
-        Button btn_kakao = findViewById(R.id.login_btn_kakao);
         ImageView main_logo = findViewById(R.id.login_iv_logo);
-
-        btn_fb.setOnClickListener(loginButtonClickListener);
-        btn_kakao.setOnClickListener(loginButtonClickListener);
         main_logo.setOnClickListener(loginButtonClickListener);
 
+        //KAKAO Auth Configuration
+        btn_kakao_login = findViewById(R.id.btn_kakao_login);
+
+        SessionCallback callback = new SessionCallback();
+        Session.getCurrentSession().addCallback(callback);
+
+        //FACEBOOK Auth Configuration
+        btn_fb_login = findViewById(R.id.fb_login_button);
+        btn_fb_login.setReadPermissions("email");
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+            new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    redirectToMain();
+                }
+
+                @Override
+                public void onCancel() {
+                    // App code
+                }
+
+                @Override
+                public void onError(FacebookException exception) {
+                    // App code
+                }
+            });
 
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void redirectToMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 
     public class LoginButtonClickListener implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.login_btn_fb:
-                    break;
-
-                case R.id.login_btn_kakao:
-                    break;
 
                 case R.id.login_iv_logo:
                     Intent intent = new Intent(v.getContext(), MainActivity.class);
@@ -47,6 +96,65 @@ public class LoginActivity extends AppCompatActivity {
                     break;
 
             }
+        }
+    }
+
+    public class SessionCallback implements ISessionCallback {
+        @Override
+        public void onSessionOpened() {
+            requestMe();
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Log.e("SessionCallback :: ", "onSessionOpenFailed : " + exception.getMessage());
+        }
+
+        // 사용자 정보 요청
+        public void requestMe() {
+            // 사용자정보 요청 결과에 대한 Callback
+            UserManagement.getInstance().requestMe(new MeResponseCallback() {
+                // 세션 오픈 실패. 세션이 삭제된 경우,
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                    Log.e("SessionCallback :: ", "onSessionClosed : " + errorResult.getErrorMessage());
+                }
+
+                // 회원이 아닌 경우,
+
+                @Override
+                public void onNotSignedUp() {
+                    Log.e("SessionCallback :: ", "onNotSignedUp");
+                }
+
+                // 사용자정보 요청에 성공한 경우,
+                @Override
+                public void onSuccess(UserProfile userProfile) {
+                    Log.e("SessionCallback :: ", "onSuccess");
+                    String nickname = userProfile.getNickname();
+                    String email = userProfile.getEmail();
+                    String profileImagePath = userProfile.getProfileImagePath();
+                    String thumnailPath = userProfile.getThumbnailImagePath();
+                    String UUID = userProfile.getUUID();
+                    long id = userProfile.getId();
+                    Log.e("Profile : ", nickname + "");
+                    Log.e("Profile : ", email + "");
+                    Log.e("Profile : ", profileImagePath  + "");
+                    Log.e("Profile : ", thumnailPath + "");
+                    Log.e("Profile : ", UUID + "");
+                    Log.e("Profile : ", id + "");
+                    redirectToMain();
+                }
+
+
+
+                // 사용자 정보 요청 실패
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    Log.e("SessionCallback :: ", "onFailure : " + errorResult.getErrorMessage());
+                }
+            });
+
         }
     }
 }
--- app/src/main/java/com/example/tmate/MainActivity.java
+++ app/src/main/java/com/example/tmate/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.tmate;
 
-import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,36 @@ public class MainActivity extends AppCompatActivity {
 
         //fab.setOnClickListener(mainButtonClickListener);
 
+
+        //kakao Map API Configuration
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        // 중심점 변경
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true);
+
+        // 줌 레벨 변경
+        mapView.setZoomLevel(7, true);
+
+        // 중심점 변경 + 줌 레벨 변경
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(33.41, 126.52), 9, true);
+
+        // 줌 인
+        mapView.zoomIn(true);
+
+        // 줌 아웃
+        mapView.zoomOut(true);
+
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633));
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+
+        mapView.addPOIItem(marker);
 
     }
 
